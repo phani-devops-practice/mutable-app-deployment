7b89d3066f3fb4d5c930d6abf2a5d3456c1d258b
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-12-25 12:39:50 +0530

    updating ./mutable-app-deployment

--- deploy.tf
+++ deploy.tf
@@ -8,7 +8,7 @@ resource "null_resource" "cluster" {
       user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
       password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
       host     = element(data.aws_instances.instances.private_ips, count.index)
-
+    }
     inline = [
       "ansible-pull -U https://github.com/phani-devops-practice/TRN-roboshop-ansilble.git roboshop.yml -e HOST=localhost -e ROLE=${var.COMPONENT} -e ENV=${var.ENV} -e DOCDB_ENDPOINT=${data.terraform_remote_state.mutable.outputs.DOCDB_ENDPOINT} -e REDIS_ENDPOINT=${data.terraform_remote_state.mutable.outputs.REDIS_ENDPOINT} -e MYSQL_ENDPOINT=${data.terraform_remote_state.mutable.outputs.MYSQL_ENDPOINT}",
     ]
